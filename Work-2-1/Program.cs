57d844da788bccc77bde4add7dc90507223b1429
liza <student@C239U30>
2020-09-04 15:00:12 +0300

    1

--- Work-2-1/Program.cs
+++ Work-2-1/Program.cs
@@ -4,18 +4,18 @@ namespace Work_2_1
 {
   class Program
   {
-    SByte a = 0;
-    Byte b = 0;
-    Int16 c = 0;
-    Int32 d = 0; 
-    Int64 e = 0; 
-    string s= "";
-    Exception ex = new Exception();
-    object[] types = { a, b, c, d, e, s, ex };
-
     static void Main(string[] args)
     {
-      foreach (object о in types)
+      SByte a = 0;
+      Byte b = 0;
+      Int16 c = 0;
+      Int32 d = 0;
+      Int64 e = 0;
+      string s = " ";
+      Exception ex = new Exception();
+      object[] types = { a, b, c, d, e, s, ex };
+
+      foreach (object o in types)
       {
         string type;
         if (o.GetType().IsValueType) type = "Value type";
---
+++ Work-2-3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Work_2_3
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
